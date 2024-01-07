b7473f5bf29443f1415fd348924861bb05fda0d1
tudor <mtb.balan@gmail.com>
2024-01-07 14:39:25 +0200

    sses started

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import fastifyEnv from "@fastify/env";
-import Fastify from "fastify";
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 import fastifyRedis from "@fastify/redis";
 import cluster from "node:cluster";
 
@@ -17,6 +17,7 @@ import { roleTable } from "./models/role.model.js";
 import { specialityTable } from "./models/speciality.model.js";
 import { userRolesMappingsTable } from "./models/userRolesMappings.model.js";
 import { doctorSpecialitiesMappingsTable } from "./models/doctorSpecialitiesMappings.js";
+import fastifyCors from "@fastify/cors";
 
 declare module "fastify" {
   interface FastifyInstance {
@@ -41,7 +42,7 @@ declare module "fastify" {
 }
 
 export const fastifyServer = Fastify({
-  logger: true,
+  // logger: true,
 });
 
 fastifyServer.get("/non-blocking", async (request, reply) => {
@@ -78,68 +79,135 @@ fastifyServer.get("/blocking", async (request, reply) => {
   return { counter };
 });
 
-const numClusterWorkers = 2;
+const numClusterWorkers = 8;
+
+const corsOptions = {
+  origin: "*",
+};
+
+// SSE
+let clients: any[] = [];
+
+function sendEventToClients(eventData: string) {
+  clients.forEach((client) => {
+    client.response.raw.write(`data: ${eventData}\n\n`);
+  });
+}
+
+function eventsHandler(req: FastifyRequest, reply: FastifyReply) {
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+    "Access-Control-Allow-Origin": `http://192.168.2.16:3000`,
+    "Access-Control-Expose-Headers": "*",
+  };
+
+  reply.raw.writeHead(200, headers);
+
+  const clientId = req.id;
+
+  //   const eventData = JSON.stringify({ notification: "Button clicked! here" });
+  //   sendEventToClients(eventData);
+
+  const newClient = {
+    id: clientId,
+    response: reply,
+  };
+
+  clients.push(newClient);
+
+  req.raw.on("close", () => {
+    console.log(`${clientId} connection closed`);
+    clients = clients.filter((client) => client.id !== clientId);
+  });
+}
+
+fastifyServer.get("/sse", eventsHandler);
+
+fastifyServer.post("/notify-clients", (req, reply) => {
+  const eventData = JSON.stringify({
+    type: "other",
+    notification: "Button clicked! here",
+  });
+  sendEventToClients(eventData);
+  reply.send("Notification sent to clients");
+});
+
+let counter = 0;
+
+fastifyServer.post("/counter", (req, reply) => {
+  counter = counter + 1;
+  const eventData = JSON.stringify({ type: "counter", counter });
+  sendEventToClients(eventData);
+  reply.send("Notification sent to clients");
+});
+// /SSE
+
 const start = async () => {
-  // try {
-  //   fastifyServer.register(fastifyEnv, options);
-  //   fastifyServer.register(fastifyRedis, {
-  //     host: "127.0.0.1",
-  //     // password: "your strong password here",
-  //     port: 6379,
-  //     family: 4,
-  //   });
-  //   await fastifyServer;
-
-  //   await migrateToDb();
-
-  //   const fastifyServerIPAddress = getServerIPAddressEnv();
-  //   const fastifyServerPort = getServerPortEnv();
-
-  //   await fastifyServer.listen({
-  //     port: fastifyServerPort,
-  //     host: fastifyServerIPAddress,
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-  // }
-
-  if (cluster.isPrimary) {
-    for (let i = 0; i < numClusterWorkers; i++) {
-      cluster.fork();
-    }
-
-    cluster.on("exit", (worker, code, signal) =>
-      console.log(`worker ${worker.process.pid} died`)
-    );
-
-    cluster.on("online", (worker) => {
-      console.log("Yay, the worker responded after it was forked");
+  try {
+    fastifyServer.register(fastifyCors, corsOptions);
+    fastifyServer.register(fastifyEnv, options);
+    fastifyServer.register(fastifyRedis, {
+      host: "127.0.0.1",
+      // password: "your strong password here",
+      port: 6379,
+      family: 4,
     });
-  } else {
-    try {
-      console.log("I am worker #" + cluster.worker?.id);
-      fastifyServer.register(fastifyEnv, options);
-      fastifyServer.register(fastifyRedis, {
-        host: "127.0.0.1",
-        // password: "your strong password here",
-        port: 6379,
-        family: 4,
-      });
-      await fastifyServer;
+    await fastifyServer;
 
-      await migrateToDb();
+    await migrateToDb();
 
-      const fastifyServerIPAddress = getServerIPAddressEnv();
-      const fastifyServerPort = getServerPortEnv();
+    const fastifyServerIPAddress = getServerIPAddressEnv();
+    const fastifyServerPort = getServerPortEnv();
 
-      await fastifyServer.listen({
-        port: fastifyServerPort,
-        host: fastifyServerIPAddress,
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    await fastifyServer.listen({
+      port: fastifyServerPort,
+      host: fastifyServerIPAddress,
+    });
+  } catch (error) {
+    console.log(error);
   }
+
+  // if (cluster.isPrimary) {
+  //   console.log(`Primary ${process.pid} is running`);
+  //   for (let i = 0; i < numClusterWorkers; i++) {
+  //     cluster.fork();
+  //   }
+
+  //   cluster.on("exit", (worker, code, signal) =>
+  //     console.log(`worker ${worker.process.pid} died`)
+  //   );
+
+  //   cluster.on("online", (worker) => {
+  //     console.log("Yay, the worker responded after it was forked");
+  //   });
+  // } else {
+  //   try {
+  //     console.log("I am worker #" + cluster.worker?.id);
+  //     fastifyServer.register(fastifyCors, corsOptions);
+  //     fastifyServer.register(fastifyEnv, options);
+  //     fastifyServer.register(fastifyRedis, {
+  //       host: "127.0.0.1",
+  //       // password: "your strong password here",
+  //       port: 6379,
+  //       family: 4,
+  //     });
+  //     await fastifyServer;
+
+  //     await migrateToDb();
+
+  //     const fastifyServerIPAddress = getServerIPAddressEnv();
+  //     const fastifyServerPort = getServerPortEnv();
+
+  //     await fastifyServer.listen({
+  //       port: fastifyServerPort,
+  //       host: fastifyServerIPAddress,
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 };
 
 start();
