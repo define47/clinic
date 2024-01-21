273d7ae435bf77118e5e55793becda68487761d8
tudor <mtb.balan@gmail.com>
2024-01-21 12:29:30 +0200

    input design continued

--- client/src/components/design/InputDesign.tsx
+++ client/src/components/design/InputDesign.tsx
@@ -1,22 +1,47 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 export const InputDesign: FC = () => {
+  const [focused, setFocused] = useState(false);
+  const [hasText, setHasText] = useState(false);
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
+  const handleBlur = () => {
+    setFocused(false);
+    if (!hasText) {
+      setFocused(false);
+    }
+  };
+
+  const handleInputChange = (event: any) => {
+    setHasText(event.target.value.trim() !== "");
+  };
+
   return (
     <div>
-      <div className="input-group">
+      {/* <div className="input-group">
         <input type="text" required />
         <label htmlFor="">username</label>
-      </div>
+      </div> */}
 
-      <div className="input-group relative my-5">
+      <div className="relative mt-20">
         <input
           type="text"
-          className="w-80 h-10 text-base text-black p-2 bg-transparent border border-black outline-none rounded-md"
+          className={`w-80 h-10 text-base text-black p-2 bg-transparent border border-black outline-none rounded-md focus:border-gray-300 transition-all duration-200 ${
+            focused || hasText ? "border-gray-300" : ""
+          }`}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onChange={handleInputChange}
           required
         />
         <label
           htmlFor=""
-          className="absolute top-2 left-2 text-base text-black px-1 bg-gray-50 pointer-events-none transition-all duration-200"
+          className={`absolute left-2 transform ${
+            focused || hasText ? "-translate-y-2 text-xs" : "translate-y-2"
+          } text-black px-1 pointer-events-none bg-white transition-all duration-200`}
         >
           Username
         </label>
