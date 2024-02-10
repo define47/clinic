34384887f9d1f082c0bb672128ac5e84bf9cdba7
tudor <mtb.balan@gmail.com>
2024-02-10 07:26:11 +0200

    dran and drop continued

--- client/src/components/DragAndDrop.tsx
+++ client/src/components/DragAndDrop.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 export const DragAndDrop: FC = () => {
   const groups = ["group1", "group2", "group3", "noDrop"];
@@ -8,5 +8,83 @@ export const DragAndDrop: FC = () => {
     { id: 3, group: "group1", value: "drag 3" },
   ];
 
-  return <div></div>;
+  const [items, setItems] = useState(initialItems);
+  const [dragData, setDragData] = useState({});
+  const [noDrop, setNoDrop] = useState("");
+
+  const handleDragStart = (e, id, group) => {
+    setDragData({ id: id, initialGroup: group });
+  };
+
+  const handleDragEnter = (e, group) => {
+    if (group === "noDrop") {
+      setNoDrop("noDrop");
+    }
+  };
+
+  // setNoDrop to nothing to return styling to normal
+  const handleDragLeave = (e) => {
+    setNoDrop("");
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const changeCategory = (itemId, group) => {
+    const newItems = [...items];
+    newItems[itemId - 1].group = group;
+    setItems([...newItems]);
+  };
+
+  const handleDrop = (e, group) => {
+    setNoDrop("");
+    const selected = dragData.id;
+    if (group === "noDrop") {
+      console.log("nuh uh");
+    } else {
+      changeCategory(selected, group);
+    }
+  };
+
+  useEffect(() => {
+    console.log("dragData", dragData);
+  }, [dragData]);
+
+  useEffect(() => {
+    console.log("noDrop", noDrop);
+  }, [noDrop]);
+
+  return (
+    <>
+      <div className="groups">
+        {groups.map((group) => (
+          <div
+            className="group"
+            onDragEnter={(e) => handleDragEnter(e, group)}
+            onDragLeave={handleDragLeave}
+            onDragOver={handleDragOver}
+            onDrop={(e) => handleDrop(e, group)}
+          >
+            <h1 className="title">{group}</h1>
+            <div>
+              {items
+                .filter((item) => item.group === group)
+                .map((item) => (
+                  <div
+                    key={item.id}
+                    id={item.id.toString()}
+                    className="item"
+                    draggable
+                    onDragStart={(e) => handleDragStart(e, item.id, group)}
+                  >
+                    {item.value}
+                  </div>
+                ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
