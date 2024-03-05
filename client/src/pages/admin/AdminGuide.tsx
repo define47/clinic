1a2ab45829be9a4e82c19634a81a04cbf0b5b9e4
tudor <mtb.balan@gmail.com>
2024-03-05 23:11:13 +0200

    continued

--- client/src/pages/admin/AdminGuide.tsx
+++ client/src/pages/admin/AdminGuide.tsx
@@ -1,10 +1,11 @@
 import { FC, useEffect, useState } from "react";
 import { DailyAppointmentsTablePrinter } from "../../components/printing/DailyAppointmentsTablePrinter";
-import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
+import ReactPDF, { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
 import { DoctorTimetablePDF } from "../../components/PDFs/DoctorTmetablePDF";
 import axios from "axios";
 import { appointmentsAPIPath } from "../../utils/dotenv";
 import { AppointmentTableData } from "../../types";
+import PDFMerger from "pdf-merger-js/browser";
 
 export const AdminGuide: FC = () => {
   const [tableRows, setTableRows] = useState<AppointmentTableData[]>([]);
@@ -25,7 +26,7 @@ export const AdminGuide: FC = () => {
             orderBy: "asc:appointmentDateTime",
             limit: 9999999,
             page: 0,
-            doctorId: "fe11da6e-91d7-54bd-b9f8-6714cb986df3",
+            doctorId: "",
             patientId: "",
           },
           withCredentials: true,
@@ -42,22 +43,126 @@ export const AdminGuide: FC = () => {
     fetchTableData();
   }, []);
 
+  const [mergedPdfUrl, setMergedPdfUrl] = useState<string>();
+  const [pdfLinks, setPdfLinks] = useState<JSX.Element[]>([]);
+
+  async function generatePDFsAllDoctors() {
+    const files: JSX.Element[] = [];
+    let tests = [];
+    const tableRowsByDoctorId: Record<string, AppointmentTableData[]> = {};
+    tableRows.forEach((tableRow) => {
+      const doctorId = tableRow.doctor.doctorId;
+      if (!tableRowsByDoctorId[doctorId]) {
+        tableRowsByDoctorId[doctorId] = [];
+      }
+      tableRowsByDoctorId[doctorId].push(tableRow);
+    });
+
+    const doctorIds = Object.keys(tableRowsByDoctorId);
+
+    for (let i = 0; i < doctorIds.length; i++) {
+      const pdf = (
+        <PDFDownloadLink
+          key={`${doctorIds[i]}`} // Make sure to provide a unique key
+          document={
+            <DoctorTimetablePDF
+              appointments={tableRowsByDoctorId[doctorIds[i]]}
+            />
+          }
+          fileName="doctor_timetable.pdf"
+        >
+          {({ blob, url, loading, error }) =>
+            loading ? "Loading document..." : "Download now!"
+          }
+        </PDFDownloadLink>
+      );
+      const blob = await ReactPDF.pdf(
+        <DoctorTimetablePDF appointments={tableRowsByDoctorId[doctorIds[i]]} />
+      ).toBlob();
+      tests.push(blob);
+      files.push(pdf);
+    }
+
+    const merger = new PDFMerger();
+    await merger.setMetadata({
+      producer: "Custom Producer",
+      author: "Custom Author",
+      creator: "Custom Creator",
+      title: "Custom Title",
+    });
+    // await merger.save("this is the name of the pdf");
+
+    for (const file of tests) {
+      await merger.add(file);
+    }
+
+    const mergedPdf = await merger.saveAsBlob();
+
+    const url = URL.createObjectURL(mergedPdf);
+
+    setMergedPdfUrl(url);
+
+    setPdfLinks(files);
+  }
+
+  useEffect(() => {
+    generatePDFsAllDoctors();
+  }, [tableRows]);
+
   return (
     <div>
-      {/* Admin Guide <RichTextEditor /> */}
-      {/* <DailyAppointmentsTablePrinter /> */}
-      <PDFDownloadLink
-        document={<DoctorTimetablePDF appointments={tableRows} />}
-        fileName="doctor_timetable.pdf"
-      >
-        {({ blob, url, loading, error }) =>
-          loading ? "Loading document..." : "Download now!"
-        }
-      </PDFDownloadLink>
+      {/* {Object.keys(tableRowsByDoctorId).map((doctorId) =>
+        tableRowsByDoctorId[doctorId].map(
+          (tableRow: AppointmentTableData, tableRowIndex: number) => (
+            <PDFDownloadLink
+              key={`${doctorId}-${tableRowIndex}`} // Make sure to provide a unique key
+              document={
+                <DoctorTimetablePDF
+                  appointments={tableRowsByDoctorId[doctorId]}
+                />
+              }
+              fileName="doctor_timetable.pdf"
+            >
+              {({ blob, url, loading, error }) =>
+                loading ? "Loading document..." : "Download now!"
+              }
+            </PDFDownloadLink>
+          )
+        )
+      )} */}
 
-      <PDFViewer width="100%" height="900px">
+      {/* <PDFViewer width="100%" height="900px">
         <DoctorTimetablePDF appointments={tableRows} />
-      </PDFViewer>
+      </PDFViewer> */}
+      <div className="w-full">
+        <div className="w-full grid grid-cols-5">
+          {pdfLinks.map((pdfLink: JSX.Element) => (
+            <div className="col-span-1">{pdfLink}</div>
+          ))}
+        </div>
+
+        <iframe
+          height={700}
+          src={`${mergedPdfUrl}`}
+          title="pdf-viewer"
+          width="100%"
+        ></iframe>
+      </div>
     </div>
+
+    // <div>
+    //   <PDFDownloadLink
+    //     document={<DoctorTimetablePDF appointments={tableRows} />}
+    //     fileName="doctor_timetable.pdf"
+    //   >
+    //     {({ blob, url, loading, error }) =>
+    //       loading ? "Loading document..." : "Download now!"
+    //     }
+    //   </PDFDownloadLink>
+
+    //   <PDFViewer width="100%" height="900px">
+    //     <DoctorTimetablePDF appointments={tableRows} />
+    //   </PDFViewer>
+    // </div>
   );
 };
